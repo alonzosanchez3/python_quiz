0edacd8475605a214b467b62b3de6613e3116123
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-01-10 20:00:00 -0600

    finished initialization of init method

--- ui.py
+++ ui.py
@@ -14,8 +14,17 @@ class QuizInterface:
     self.score_label.grid(row=0, column=1)
 
     self.canvas = tkinter.Canvas(width=300, height=250, bg='white')
-    self.question_text = self.canvas.create_text(150, 125, text="Some question text", fill=THEME_COLOR)
-    self.canvas.grid(row=1, column=0, columnspan=2)
+    self.question_text = self.canvas.create_text(150, 125, text="Some question text", fill=THEME_COLOR, font=('Arial', 20, 'italic'))
+    self.canvas.grid(row=1, column=0, columnspan=2,pady=50)
+
+    true_image = tkinter.PhotoImage(file='./images/true.png')
+    self.true_button = tkinter.Button(image=true_image, highlightthickness=0)
+    self.true_button.grid(row=2, column=0)
+
+    false_image = tkinter.PhotoImage(file='./images/false.png')
+    self.false_button = tkinter.Button(image=false_image, highlightthickness=0)
+    self.false_button.grid(row=2, column=1)
+
 
 
     self.window.mainloop()
