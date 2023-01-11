036eb1924132b2a7d6a0065dba92794dfe4710aa
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-01-10 20:00:00 -0600

    implemented init method

--- ui.py
+++ ui.py
@@ -8,5 +8,14 @@ class QuizInterface:
   def __init__(self):
     self.window = tkinter.Tk()
     self.window.title('Quizzler')
+    self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+    self.score_label = tkinter.Label(text="Score: 0", fg='white', bg=THEME_COLOR)
+    self.score_label.grid(row=0, column=1)
+
+    self.canvas = tkinter.Canvas(width=300, height=250, bg='white')
+    self.question_text = self.canvas.create_text(150, 125, text="Some question text", fill=THEME_COLOR)
+    self.canvas.grid(row=1, column=0, columnspan=2)
+
 
     self.window.mainloop()
