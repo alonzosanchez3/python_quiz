3d322b3feaa2a38bb76db46a508e7d4d1dc46e70
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-01-13 20:00:00 -0600

    initialized ui class

--- ui.py
+++ ui.py
@@ -1,2 +1,12 @@
+import tkinter
+
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+
+  def __init__(self):
+    self.window = tkinter.Tk()
+    self.window.title('Quizzler')
+
+    self.window.mainloop()
