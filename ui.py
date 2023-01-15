5e704f16195b5e0630ee162e09c57dca8b437654
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-01-14 20:00:00 -0600

    added button functionality

--- ui.py
+++ ui.py
@@ -1,11 +1,13 @@
 import tkinter
+from quiz_brain import QuizBrain
 
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
 
-  def __init__(self):
+  def __init__(self, quiz_brain:QuizBrain):
+    self.quiz = quiz_brain
     self.window = tkinter.Tk()
     self.window.title('Quizzler')
     self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -14,17 +16,31 @@ class QuizInterface:
     self.score_label.grid(row=0, column=1)
 
     self.canvas = tkinter.Canvas(width=300, height=250, bg='white')
-    self.question_text = self.canvas.create_text(150, 125, text="Some question text", fill=THEME_COLOR, font=('Arial', 20, 'italic'))
+    self.question_text = self.canvas.create_text(150, 125, width=280, text="Some question text", fill=THEME_COLOR, font=('Arial', 20, 'italic'))
     self.canvas.grid(row=1, column=0, columnspan=2,pady=50)
 
     true_image = tkinter.PhotoImage(file='./images/true.png')
-    self.true_button = tkinter.Button(image=true_image, highlightthickness=0)
+    self.true_button = tkinter.Button(image=true_image, highlightthickness=0, command=self.answer_true)
     self.true_button.grid(row=2, column=0)
 
     false_image = tkinter.PhotoImage(file='./images/false.png')
-    self.false_button = tkinter.Button(image=false_image, highlightthickness=0)
+    self.false_button = tkinter.Button(image=false_image, highlightthickness=0, command=self.answer_false)
     self.false_button.grid(row=2, column=1)
+
+    self.get_next_question()
 
 
 
     self.window.mainloop()
+
+  def get_next_question(self):
+    question_text = self.quiz.next_question()
+    self.canvas.itemconfig(self.question_text, text=question_text)
+
+  def answer_false(self):
+    self.quiz.check_answer("False")
+    self.get_next_question()
+
+  def answer_true(self):
+    self.quiz.check_answer("True")
+    self.get_next_question()
